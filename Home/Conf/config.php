c8841650976e16d121f1ee391507b6f187024877
unknown <chrisdowson.duan@PC-DXIAOQIANG.jinfuzi.ad>
2016-01-12 14:39:08 +0800

    init commit

--- Home/Conf/config.php
+++ Home/Conf/config.php
@@ -8,7 +8,7 @@ return array(
 		'DB_HOST'   => 'localhost', // 服务器地址
 		'DB_NAME'   => 'gxlm', // 数据库名
 		'DB_USER'   => 'root', // 用户名
-		'DB_PWD'    => '123456', // 密码
+		'DB_PWD'    => '', // 密码
 		'DB_PORT'   => 3306, // 端口
 		'DB_PREFIX' => '',// 数据库表前缀
 		//其他项目配置参数
