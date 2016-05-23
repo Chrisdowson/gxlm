783f8a3d6fae25f2edb85fcbb1d119c4fbde9018
chrisdowson <1101225304@qq.com>
2016-05-23 16:20:03 +0800

    del

--- Application/Admin/Controller/IndexController.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Admin\Controller;
-use Think\Controller;
-use \Admin\Common\CommonController;
-class IndexController extends CommonController {
-    public function index(){
-        $this->display();
-    }
-}
--- Application/Admin/Model/AdminModel.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace Admin\Model;
-use Think\Model;
-class AdminModel extends Model{
-    protected $tableName = 'admin';
-}
